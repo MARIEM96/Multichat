681c60ebb94fb68df8e407f76b600c8ee0596734
MARIEM96 <43937474+MARIEM96@users.noreply.github.com>
2020-09-22 10:55:19 +0100

    Update multichat.ex

--- lib/multichat.ex
+++ lib/multichat.ex
@@ -1,13 +1,9 @@
 defmodule Multichat do
   import Chat.Nodes
-
-
   use GenServer
 
   def start_link(args) do
-
     name = {:global, :"#{node()}-chat"} |> IO.inspect(label: "Name")
-
     GenServer.start_link(__MODULE__, args, name: name)
   end
 
@@ -49,16 +45,12 @@ defmodule Multichat do
 
 
   def send_message(message) do
-
-
     for noeud <- Node.list()|> IO.inspect(label: "Destinataire") do
       GenServer.cast({:global, :"#{noeud}-chat"}, {:receive, message})
     end
-
   end
 
   def handle_cast({:receive, message}, state) do
-
     IO.puts(message)
     {:noreply, state}
   end
