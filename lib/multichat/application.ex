f5ca56495ad2fd65a26105651ad0e1b433a9fbb9
MARIEM96 <43937474+MARIEM96@users.noreply.github.com>
2020-09-22 10:52:52 +0100

    Update application.ex

--- lib/multichat/application.ex
+++ lib/multichat/application.ex
@@ -1,21 +1,22 @@
 defmodule Multichat.Application do
 
-  use Application
+    use Application
 
 
-  def start(_type, _args) do
-    port = String.to_integer(System.get_env("PORT") || "4001")
+    def start(_type, _args) do
+      port = String.to_integer(System.get_env("PORT") || "4001")
+      :pg2.create :clients
+      
+      children = [
+        Multichat,
+        Multichat.Repo,
+        {Task, &Multichat.connect/0},
+        {Task.Supervisor, name: Multichat.Server.ConnectionSupervisor},
+         Supervisor.child_spec({Task, fn -> Multichat.Server.accept(port) end}, id: :dynamic1)
+       ]
 
-    children = [
-      Multichat,
-      Multichat.Repo,
-      {Task, &Multichat.connect/0},
-      {Task.Supervisor, name: Multichat.Server.ConnectionSupervisor},
-       Supervisor.child_spec({Task, fn -> Multichat.Server.accept(port) end}, id: :dynamic1)
-     ]
+      opts = [strategy: :one_for_one, name: Multichat.Supervisor]
+      Supervisor.start_link(children, opts)
 
-    opts = [strategy: :one_for_one, name: Multichat.Supervisor]
-    Supervisor.start_link(children, opts)
-
-end
+  end
 end
